3f4dd1ff996b2f74c562c34c568173c2d414451c
sohailkeele987 <sohailakhtar303207@gmail.com>
2026-03-12 22:42:41 +0500

    added loop so player can keep guessing untill correct

--- game.py
+++ game.py
@@ -1,12 +1,16 @@
 import random
 
 number = random.randint(1, 10)
+guess = None
 
 print("Welcome to Guess the Number Game")
 
-guess = int(input("Enter a number between 1 and 10: "))
+while guess != number:
+    guess = int(input("Enter a number between 1 and 10: "))
 
-if guess == number:
-    print("Correct! You win!")
-else:
-    print("Wrong! The number was", number)
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("Correct! You win!")
