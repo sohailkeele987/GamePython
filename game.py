40d4cbe88c5542b40030362ed7bcc6504c33e369
sohailkeele987 <sohailakhtar303207@gmail.com>
2026-03-12 22:43:24 +0500

    Added Hints for higher or lower guesses

--- game.py
+++ game.py
@@ -2,15 +2,17 @@ import random
 
 number = random.randint(1, 10)
 guess = None
+attempts = 0
 
 print("Welcome to Guess the Number Game")
 
 while guess != number:
     guess = int(input("Enter a number between 1 and 10: "))
+    attempts += 1
 
     if guess < number:
         print("Too low!")
     elif guess > number:
         print("Too high!")
     else:
-        print("Correct! You win!")
+        print("Correct! You win in", attempts, "attempts!")
